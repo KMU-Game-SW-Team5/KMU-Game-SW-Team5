6028fdbba58b9ac9ff8d09b2672ef3756346a6f9
박현빈 <empty6004@kookmin.ac.kr>
2025-11-04 16:40:26 +0900

    벽 프리팹 생성

--- Assets/Script/MapMaker.cs
+++ Assets/Script/MapMaker.cs
@@ -5,27 +5,79 @@ using UnityEngine;
 public class MapMaker : MonoBehaviour
 {
     public int n = 10;              // 생성할 방의 개수
-    public int floorSize = 1;       // 바닥 크기
+    public int roomSize = 1;        // 방 크기
     public GameObject floor;        // 바닥 프리팹
-    public int wallSize = 1;        // 벽 크기 일단, floor Size와는 통일할 예정이다.
-    public GameObject wall;
+    public GameObject wall;         // 벽 프리팹 
+    public GameObject wallDoor;     // 문이 있는 벽 프리팹 
 
     private HashSet<Vector3> PositionSet = new HashSet<Vector3>();
     private Queue<Vector3> PositionQueue = new Queue<Vector3>();
-
-    private Vector3 currentPosition = new Vector3(0, 0, 0); // 시작 지점
     private Vector3 nextPosition;   // 새로 생성될 방 위치
 
     void Start()
     {
-        PlaneMaker();
+        FloorMaker();
     }
 
-    void PlaneMaker()
-    {
-        // 시작 지점 초기화
-        PositionQueue.Enqueue(currentPosition);
-        PositionSet.Add(currentPosition);
+    void FloorMaker()
+    {   
+        // 큐와 집합 초기화 및 초기값 설정
+        PositionQueue.Clear();
+        PositionSet.Clear();
+        PositionQueue.Enqueue(new Vector3(0, 0, 0));
+        PositionSet.Add(new Vector3(0, 0, 0));
+
+        while (PositionQueue.Count != 0)
+        {
+            // 큐에서 위치 꺼내기
+            Vector3 nowPosition = PositionQueue.Dequeue();
+
+            // 방생성
+            GameObject newfloor = Instantiate(floor, currentPosition, Quaternion.identity);
+            newfloor.transform.localScale = new Vector3(roomSize, 1, roomSize);
+
+            // 상하좌우 방향 리스트
+            List<string> UDLRlist = new List<string> { "Up", "Down", "Left", "Right" };
+
+            for (int i = 0; i < 4; i++)
+            {
+                // 무작위 인덱스 선택
+                int index = Random.Range(0, UDLRlist.Count);
+                string selected = UDLRlist[index];
+
+                // 방향에 따른 위치 계산
+                switch (selected)
+                {
+                    case "Up":
+                        nextPosition = new Vector3(currentPosition.x, 0, currentPosition.z + (10 * roomSize));
+                        break;
+                    case "Down":
+                        nextPosition = new Vector3(currentPosition.x, 0, currentPosition.z - (10 * roomSize));
+                        break;
+                    case "Left":
+                        nextPosition = new Vector3(currentPosition.x - (10 * roomSize), 0, currentPosition.z);
+                        break;
+                    case "Right":
+                        nextPosition = new Vector3(currentPosition.x + (10 * roomSize), 0, currentPosition.z);
+                        break;
+                }
+
+                // 사용한 방향 제거
+                UDLRlist.RemoveAt(index);
+
+                // 접근한적 없는 위치인 경우
+                if (!PositionSet.Contains(nextPosition) && n != 0)
+                {
+                    if (Random.value < 0.5f)
+                    {
+                        n--;
+                        PositionQueue.Enqueue(nextPosition);
+                        PositionSet.Add(nextPosition);
+                    }
+                }
+            }
+
+        }
 
         while (n > 0 && PositionQueue.Count > 0)
         {
@@ -34,7 +86,7 @@ public class MapMaker : MonoBehaviour
 
             // 방 생성
             GameObject newfloor = Instantiate(floor, currentPosition, Quaternion.identity);
-            newfloor.transform.localScale = new Vector3(floorSize, 1, floorSize);
+            newfloor.transform.localScale = new Vector3(roomSize, 1, roomSize);
             n--;
 
             // 상하좌우 방향 리스트
@@ -50,20 +102,23 @@ public class MapMaker : MonoBehaviour
                 switch (selected)
                 {
                     case "Up":
-                        nextPosition = new Vector3(currentPosition.x, 0, currentPosition.z + (10 * floorSize));
+                        nextPosition = new Vector3(currentPosition.x, 0, currentPosition.z + (10 * roomSize));
                         break;
                     case "Down":
-                        nextPosition = new Vector3(currentPosition.x, 0, currentPosition.z - (10 * floorSize));
+                        nextPosition = new Vector3(currentPosition.x, 0, currentPosition.z - (10 * roomSize));
                         break;
                     case "Left":
-                        nextPosition = new Vector3(currentPosition.x - (10 * floorSize), 0, currentPosition.z);
+                        nextPosition = new Vector3(currentPosition.x - (10 * roomSize), 0, currentPosition.z);
                         break;
                     case "Right":
-                        nextPosition = new Vector3(currentPosition.x + (10 * floorSize), 0, currentPosition.z);
+                        nextPosition = new Vector3(currentPosition.x + (10 * roomSize), 0, currentPosition.z);
                         break;
                 }
 
-                // 새로운 위치라면 50% 확률로 추가
+                // 사용한 방향 제거
+                UDLRlist.RemoveAt(index);
+
+                // 접근한적 없는 위치인 경우
                 if (!PositionSet.Contains(nextPosition))
                 {
                     if (Random.value < 0.5f)
@@ -72,15 +127,12 @@ public class MapMaker : MonoBehaviour
                         PositionSet.Add(nextPosition);
                     }
                 }
-
-                // 사용한 방향 제거
-                UDLRlist.RemoveAt(index);
             }
         }
     }
     
     void WallMaker()
     {
-        
+        //(0, 0)을 기준으로 상하좌우 좌표에 접근하여 
     }
 }
