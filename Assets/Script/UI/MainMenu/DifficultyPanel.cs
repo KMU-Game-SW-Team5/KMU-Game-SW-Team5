c84d7daf907215f2eb3b6af3794a4cf93e91d20f
Jeon-kj <jeonkj0413@naver.com>
2025-11-12 22:55:24 +0900

    Feat(UI): Start 클릭 시 난이도 선택
    - 메인 메뉴에서 Start 클릭 시 난이도 선택 가능하도록 수정
    
    - MainPanel → MenuPanel로 변경
    - 패널 증가에 따른 MainMenuController 기능 분리

---
+++ Assets/Script/UI/MainMenu/DifficultyPanel.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DifficultyPanel : MonoBehaviour
+{
+    [SerializeField] private MainMenuController mmc;
+
+    public void OnClickDifficulty(int idx)
+    {
+        SettingsService.GameDifficulty = idx;
+        SettingsService.Save();
+
+        StartGame();
+    }
+
+    public void OnClickBack() => mmc?.ShowMenuPanel();
+
+
+    private void StartGame()
+    {
+        /*
+        // GameScene이 빌드 세팅에 포함되어 있어야 함
+        if (Application.CanStreamedLevelBeLoaded("GameScene"))
+            SceneManager.LoadScene("GameScene");
+        else
+            Debug.LogWarning("GameScene이 Build Settings에 등록되어 있지 않습니다");
+        */
+        SceneManager.LoadScene("Assets/Scenes/MapMakerScene.unity");
+    }
+}
